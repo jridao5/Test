d9512826ba1815cd9a1b6237c73acaaa3d762a23
jridao5 <jridao5@students.d125.org>
2015-05-23 21:05:10 -0500

    edited JSONTest and Useful URLs

--- FileNameReader/src/filenamereader/FileNameReader.java
+++ FileNameReader/src/filenamereader/FileNameReader.java
@@ -7,28 +7,8 @@ package filenamereader;
 
 import java.io.File;
 import java.io.IOException;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.effect.BlendMode;
-import javafx.scene.effect.BoxBlur;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-import static java.lang.Math.random;
-import java.nio.file.Files;
-import javax.swing.JFileChooser;
-
 
 /**
  *
@@ -44,19 +24,13 @@ public class FileNameReader extends Application
     
     @Override
     public void start(Stage primaryStage) throws IOException 
-    {  
-
-        
-        
-        
-        
-        
-        File folder = new File("/Users/jridao5/Documents/Test/Movies"); //makes a path to the specified folder
+    {   
+        File folder = new File("/Users/thejuandesire/Documents/Rafael/Test/Movies"); //makes a path to the specified folder
         File[] listOfFiles = folder.listFiles(); //makes an arrayList of file objects
         
         for (File movie : listOfFiles)
         {
-                String title = movie.getName().substring(0,movie.getName().indexOf("."));
+                String title = movie.getName().substring(0,movie.getName().indexOf("("));
                 System.out.println("Title: " + title); //title.getName() gets the title of the file
         }
     }
--- JSONTest/src/jsontest/JSONTest.java
+++ JSONTest/src/jsontest/JSONTest.java
@@ -7,6 +7,7 @@ package jsontest;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -26,28 +27,37 @@ public class JSONTest extends Application {
     
     @Override
     public void start(Stage primaryStage) throws MalformedURLException, UnsupportedEncodingException, IOException {
-        String selectedItem = "Pirates of the Caribbean: Curse of the Black Pearl"; //Change this to a title of a movie to test
+        File folder = new File("/Users/thejuandesire/Documents/Rafael/Test/Movies"); //makes a path to the specified folder
+        File[] listOfFiles = folder.listFiles(); //makes an arrayList of file objects
+        
+        for (File movie:listOfFiles)
+        {
+            String movieTitle = movie.getName().substring(0,movie.getName().indexOf("(")); //Change this to a title of a movie to test
+          
+            InputStream input = new URL("http://www.omdbapi.com/?t=" + URLEncoder.encode(movieTitle, "UTF-8")).openStream();
+            //String site = new URL("http://www.omdbapi.com/?t=" + URLEncoder.encode(movieTitle, "UTF-8")).toString();
+            //System.out.println(site);
+            
+            Map<String, String> map = new Gson().fromJson(new InputStreamReader(input, "UTF-8"), new TypeToken<Map<String, String>>(){}.getType());
 
-        InputStream input = new URL("http://www.omdbapi.com/?t=" + URLEncoder.encode(selectedItem, "UTF-8")).openStream();
-        Map<String, String> map = new Gson().fromJson(new InputStreamReader(input, "UTF-8"), new TypeToken<Map<String, String>>(){}.getType());
-
-        String title = map.get("Title");
-        String year = map.get("Year");
-        String released = map.get("Released");
-        String runtime = map.get("Runtime");
-        String genre = map.get("Genre");
-        String actors = map.get("Actors");
-        String plot = map.get("Plot");
-        String imdbRating = map.get("imdbRating");
-        String poster = map.get("Poster");
-        printStuff(title, year, released, runtime, genre, actors, plot, imdbRating, poster);
+            String title = map.get("Title");
+            String released = map.get("Released");
+            String runtime = map.get("Runtime");
+            String genre = map.get("Genre");
+            String actors = map.get("Actors");
+            String plot = map.get("Plot");
+            String imdbRating = map.get("imdbRating");
+            String poster = map.get("Poster");
+            printStuff(title, released, runtime, genre, actors, plot, imdbRating, poster);  
+        }
+        
     }
     
-    public void printStuff(String title, String year, String released, String runtime, 
+    public void printStuff(String title, String released, String runtime, 
                             String genre, String actors, String plot, String imdbRating, String poster){
-        System.out.println("Title: " + title + "\nYear: " + year + "\nReleased: " + released + 
-                            "\nRuntime: " + runtime + "\nGenre: "+ genre + "\nActors :" + actors +
-                            "\nPlot: " + plot + "\nimdbRating: " + imdbRating + "\nPoster: " + poster);
+        System.out.println("Title: " + title + "\nReleased: " + released + 
+                            "\nRuntime: " + runtime + "\nGenre: "+ genre + "\nActors: " + actors +
+                            "\nPlot: " + plot + "\nimdbRating: " + imdbRating + "\nPoster: " + poster + "\n");
     }
 
     /**
